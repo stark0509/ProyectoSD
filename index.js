c35312d97ae9c5850f27144492de4046fc5e4ab9
Stark0599 <mendozacastillomiguel@gmail.com>
2020-11-18 00:59:43 -0500

    commit with real temperature

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const { Socket } = require('socket.io');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
@@ -9,22 +8,35 @@ app.set('port',3000||process.env.PORT);
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/:id',(req,res)=>{
-  console.log(req.params['id']);
-  res.send();
-});
-
 http.listen(app.get('port'),()=>{
-  console.log('Listen on port',app.get('port'));
+  console.log('Web server listen on port',app.get('port'));
 });
+//creando el servidor mqtt
+const aedes = require('aedes')
+const server = require('net').createServer(aedes.handle)
+const port = 1883
+server.listen(port,function(){
+  console.log('MQTT server listen on port',port);
+})
+//creando el cliente que servira de publicador y subscriptor
+client.on('connect',()=>{
+  client.subscribe('temperature')//suscribiendo nuestro cliente a los datos de temperatura de Arduino
+})
 
 //websocket
 io.on('connection',(socket)=>{
   console.log('A new user connected',socket.id);
+  client.on('message',(topic,message)=>{//configurando el cliente para que reciba la Temp y la envia al navegador
+    message = message.toString();
+    data = parseFloat(message);
+    if(topic==="temperature"){
+      socket.emit('temperature',data)
+      console.log("data was sent! ",data)
+    }
+  })
   let a;
   setInterval(()=>{
     a=(Math.random()*20)+20;
-    socket.emit('temperature',a)
     socket.emit('distance',a)
   },2000);
   socket.on('enableAutomaticFan',(data)=>{
