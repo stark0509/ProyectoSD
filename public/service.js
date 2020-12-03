ee8ba4bc72f3f77f2e2d80e269a25641e955d30c
Stark0599 <mendozacastillomiguel@gmail.com>
2020-12-03 10:26:03 -0500

    deploy to heroku

--- public/service.js
+++
@@ -1,9 +0,0 @@
-const socket = io();
-socket.on('test',(data)=>{
-    console.log(data)
-})
-socket.on('data',(data)=>{
-    console.log(data);
-})
-
-"tengo que ver la manera de que de este js le pase los datos al archivo chart"
