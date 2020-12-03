34a73c0955a106c5bfe408bf7905a7d28bbf9b56
Stark0599 <mendozacastillomiguel@gmail.com>
2020-12-03 14:16:34 -0500

    with mqtt server

--- index.js
+++ index.js
@@ -20,8 +20,15 @@ server.listen(port,function(){
 })
 //creando el cliente que servira de publicador y subscriptor
 const mqtt = require('mqtt')
-const client = mqtt.connect('mqtt://localhost:1883')
+const options = {
+  port: 1883,
+  clientId: 'webServer',
+  username: 'test',
+  password: 'test',
+};
+const client = mqtt.connect('mqtt://node02.myqtthub.com:1883',options)
 client.on('connect',()=>{
+  console.log('connected to mqtt server')
   client.subscribe('temperature')//suscribiendo nuestro cliente a los datos de temperatura de Arduino
   console.log('cliente MQTT fue suscrito a los datos de temperatura!')
   client.subscribe('distance')//suscribiendo nuestro cliente a los datos de temperatura de Arduino
